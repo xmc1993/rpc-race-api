bd418982ad34a9a2fb1e64ea9e1f5dfe6c93891d
xmc1993 <1170143845@qq.com>
2015-07-28 09:51:20 +0800

    init project

---
+++ src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcConsumerImpl.java
@@ -0,0 +1,9 @@
+package com.alibaba.middleware.race.rpc.api.impl;
+
+import com.alibaba.middleware.race.rpc.api.RpcConsumer;
+
+/**
+ * Created by Administrator on 2015/7/28.
+ */
+public class RpcConsumerImpl extends RpcConsumer{
+}
---
+++ src/main/java/com/alibaba/middleware/race/rpc/api/impl/RpcProviderimpl.java
@@ -0,0 +1,9 @@
+package com.alibaba.middleware.race.rpc.api.impl;
+
+import com.alibaba.middleware.race.rpc.api.RpcProvider;
+
+/**
+ * Created by Administrator on 2015/7/28.
+ */
+public class RpcProviderimpl extends RpcProvider {
+}
