a475192c2ad3e51f228a046c70828ff19a894fd8
ivan.wang <ivan.wang@dianping.com>
2015-08-05 22:13:50 +0800

    copy files

---
+++ src/main/java/com/alibaba/middleware/race/rpc/aop/ProviderHookImpl.java
@@ -0,0 +1,20 @@
+package com.alibaba.middleware.race.rpc.aop;
+
+import com.alibaba.middleware.race.rpc.context.RpcContext;
+import com.alibaba.middleware.race.rpc.model.RpcRequest;
+
+/**
+ * Created by ivanmerlin on 2015/8/4.
+ */
+public class ProviderHookImpl implements ProviderHook {
+
+    @Override
+    public void before(RpcRequest request) {
+
+    }
+
+    @Override
+    public void after(RpcRequest request) {
+
+    }
+}
